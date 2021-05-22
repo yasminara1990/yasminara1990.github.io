c03947df83a1e710ad1082a51b62516f87aba63e
yasminara1990 <yasmin.ara1990@gmail.com>
2021-05-22 01:57:57 -0500

    Inventory Management Systems
    Inventory Management Systems

---
+++ inventory/includes/config.php
@@ -0,0 +1,20 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| OWSA-INV V2
+|--------------------------------------------------------------------------
+| Author: Siamon Hasan
+| Project Name: OSWA-INV
+| Version: v2
+| Offcial page: http://oswapp.com/
+| facebook Page: https://www.facebook.com/oswapp
+|
+|
+|
+*/
+  define( 'DB_HOST', 'localhost' );          // Set database host
+  define( 'DB_USER', 'jezzie' );             // Set database user
+  define( 'DB_PASS', 'pagolmon' );             // Set database password
+  define( 'DB_NAME', 'inventory' );        // Set database name
+
+?>
